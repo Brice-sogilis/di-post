ff2e12a06bf4c879b4d0404b9404e6321f4e3ce1
NyuB <brice.decaestecker@gmx.fr>
2024-01-11 08:57:32 +0100

    Integrated the zig memory test example

--- zig/example.zig
+++ zig/example.zig
@@ -22,3 +22,25 @@ test "caesarCiphered" {
 
     try std.testing.expectEqualStrings("Sogilis rocks!", ciphered);
 }
+
+test "This would pass" {
+    var list = std.ArrayList(i32).init(std.testing.allocator); // here we inject the testing allocator, which will track all memory allocations performed by list
+    defer list.deinit(); // ensure list memory will be freed at the end of the scope
+    try list.append(42);
+    try std.testing.expect(list.items[0] == 42);
+}
+
+test "Detecting a memory leak" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    try list.append(42);
+    try std.testing.expect(list.items[0] == 42);
+
+    // list was not freed !
+    std.debug.print("Expected memory leaks logs here, keeep calm ===> \n", .{});
+    const detectLeak = std.testing.allocator_instance.detectLeaks();
+    std.debug.print("\n<=== End of expected memory leaks logs", .{});
+    try std.testing.expect(detectLeak == true);
+
+    // if we do not actually free the list, the test would fail, helping us detecting memory leaks at test time without additionnal tools
+    list.deinit();
+}
